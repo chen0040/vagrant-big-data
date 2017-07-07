70b4cedda6ffaa28ee9162d8030926c08ae67d53
xschen <xs0040@gmail.com>
2017-07-07 23:33:02 +0800

    allow ssh passwordless during vagrant up

--- spark/devops/spark/passwordless-ssh-setup.sh
+++ spark/devops/spark/passwordless-ssh-setup.sh
@@ -3,4 +3,5 @@
 ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@sparkslave1
 ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@sparkslave2
 ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@sparkslave3
+ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@sparkmaster1
 
